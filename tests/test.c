01e8aab0b6ca0ec905fc0dbe1695b4ba9c796a00
vcn <cardoso.victore@gmail.com>
2020-12-24 08:15:45 -0800

    Adding empty functions for dependency bindings in test

--- tests/test.c
+++ tests/test.c
@@ -11,8 +11,15 @@ enum {
 
 static int load_points(vtsp_points_t *input);
 static int allocate_output(const vtsp_points_t *input, vtsp_perm_t *output);
-static int set_dependencies(vtsp_depend_t *depend);
-	
+static int bind_dependencies(vtsp_depend_t *depend);
+static int bind_logger(vtsp_binding_logger_t *logger);
+static int bind_drawer(vtsp_binding_drawer_t *drawer);
+static int bind_reporter(vtsp_binding_reporter_t *reporter);
+static int bind_envelope(vtsp_binding_envelope_t *envelope);
+static int bind_mesher(vtsp_binding_mesher_t *mesher);
+static int bind_heat(vtsp_binding_heat_t *heat);
+static int bind_integral(vtsp_binding_integral_t *integral);
+
 int main(int argc, const char* argv[])
 {
 	
@@ -28,7 +35,7 @@ int main(int argc, const char* argv[])
 	TRY( allocate_output(&input, &output) );
 
 	vtsp_depend_t depend;
-	TRY( set_dependencies(&depend) );
+	TRY( bind_dependencies(&depend) );
 	
 	TRY( vtsp_solve(&input, &output, &depend, mem) );
 	return SUCCESS;
@@ -58,14 +65,56 @@ static int allocate_output(const vtsp_points_t *input, vtsp_perm_t *output)
 	return SUCCESS;
 }
 
-static int set_dependencies(vtsp_depend_t *depend)
+static int bind_dependencies(vtsp_depend_t *depend)
 {
-	//depend->logger;
-	//depend->drawer;
-	//depend->reporter;
-	//depend->envelope;
-	//depend->mesher;
-	//depend->heat;
-	//depend->integral;
+	TRY( bind_logger(&(depend->logger)) );
+	TRY( bind_drawer(&(depend->drawer)) );
+	TRY( bind_reporter(&(depend->reporter)) );
+	TRY( bind_envelope(&(depend->envelope)) );
+	TRY( bind_mesher(&(depend->mesher)) );
+	TRY( bind_heat(&(depend->heat)) );
+	TRY( bind_integral(&(depend->integral)) );
+	return SUCCESS;
+}
+
+static int bind_logger(vtsp_binding_logger_t *logger)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_drawer(vtsp_binding_drawer_t *drawer)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_reporter(vtsp_binding_reporter_t *reporter)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_envelope(vtsp_binding_envelope_t *envelope)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_mesher(vtsp_binding_mesher_t *mesher)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_heat(vtsp_binding_heat_t *heat)
+{
+	// Bind
+	return SUCCESS;
+}
+
+static int bind_integral(vtsp_binding_integral_t *integral)
+{
+	// Bind
 	return SUCCESS;
 }
